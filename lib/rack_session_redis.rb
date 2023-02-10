e77fdd2dae616878abe17b56fd64ab6f8ab7e03d
Kenneth Teh <kennethteh90@gmail.com>
2023-02-10 15:05:11 +0800

    Require rack/session/redis and redis/store

--- lib/rack_session_redis.rb
+++ lib/rack_session_redis.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'rack_session_redis/version'
+require_relative 'rack/session/redis'
+require_relative 'redis/store'
 
 module RackSessionRedis
-  class Error < StandardError; end
-  # Your code goes here...
 end
